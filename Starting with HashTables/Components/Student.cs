0ef67947f7c80cc7b9309ffc91b24702133a6462
Si-ja <nicksemion@gmail.com>
2021-06-14 23:49:40 +0300

    Removing unused "using" imports

--- Starting with HashTables/Components/Student.cs
+++ Starting with HashTables/Components/Student.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Starting_with_HashTables.Components
+﻿namespace Starting_with_HashTables.Components
 {
     struct Student
     {
